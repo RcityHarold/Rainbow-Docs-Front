a841ee8478e692ad8c7e0f73cc9a4b5387fde0cf
A <A@A.com>
2025-07-28 16:05:33 +0800

    x

--- src/pages/notifications/index.tsx
+++ src/pages/notifications/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Card } from 'antd'
 import NotificationList from '@/components/notifications/NotificationList'
-import MainLayout from '@/components/layout/MainLayout'
 
 const NotificationsPage: React.FC = () => {
   return (
-    <MainLayout>
-      <div className="max-w-4xl mx-auto p-6">
-        <Card className="shadow-sm">
-          <NotificationList />
-        </Card>
-      </div>
-    </MainLayout>
+    <div className="max-w-4xl mx-auto p-6">
+      <Card className="shadow-sm">
+        <NotificationList />
+      </Card>
+    </div>
   )
 }
 
